6dd613599e9d534c8e3107ba748386d71dcf37fe
kaushalendra2009 <59788987+kaushalendra2009@users.noreply.github.com>
2020-01-12 13:35:12 +0530

    Add files via upload

---
+++ kaushal.R
@@ -0,0 +1,17 @@
+airquality<-datasets::airquality
+haed(airquality,5)
+head(airquality,5)
+summary(airquality[,3])
+summary(airquality$Temp)
+summ(ary(airquality$Ozone)
+plot(airquality$Temp)
+plot(airquality$temp
+     
+     
+)
+plot(airquality)
+plot(airquality$Ozone,type= "b")
+barplot(airquality$Ozone, xlab ='ozone concentration',ylab ='no of instance',main ='ozone levels in ny city',col = 'red',horiz = TRUE)
+hist(airquality$Solar.R, col = 'red')
+boxplot(airquality)
+boxplot(airquality[,1:4])
